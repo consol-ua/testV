49ca3d449afde09a9fc2dd2e0f587ed939bffee3
Ivan Malyhin <ivan.malyhin.2021@gmail.com>
2025-07-25 12:24:47 +0300

    add ReactQueryDevtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './globals.css';
 
@@ -9,6 +11,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
